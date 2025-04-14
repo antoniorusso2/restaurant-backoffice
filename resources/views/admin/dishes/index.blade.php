078c5f4d42f29be4e7f551c7fad6c8b36045ea0d
antoniorusso2 <russoantonio303@outlook.it>
2025-04-14 12:29:28 +0200

    Add dishes index page

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -7,15 +7,16 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg sm:grid xs:flex sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 @foreach ($dishes as $dish)
-                    <div class="card p-5">
+                    <div class="card h-60 p-5 border-gray-400 border flex flex-col">
                         <div class="card-head">
-                            {{ $dish->name }}
+                            <h3 class="text-2xl font-semibold">{{ $dish->name }}</h3>
                         </div>
-                        <div class="card-body">
-                            {{ $dish->description }}
+                        <div class="card-body flex flex-col h-full text-nowrap">
+                            <p class="overflow-hidden text-ellipsis h-25">{{ $dish->description }}</p>
                         </div>
+                        <a class="btn special align-self-end max-w-min" href="{{ route('dishes.show', $dish) }}">Dettagli</a>
                     </div>
                 @endforeach
             </div>
