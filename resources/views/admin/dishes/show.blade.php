d751ece9ebcf52cf71343331718738b83a383e3c
antoniorusso2 <russoantonio303@outlook.it>
2025-04-16 17:57:34 +0200

    fix btn width

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,11 +1,5 @@
 <x-app-layout>
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="pt-12 max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-between">
         <div class="w-full flex gap-x-5">
             <a class="btn special" href="{{ route('dishes.index') }}">Indietro</a>
