4756746a46a0da7336eb208620e99988d4a54b02
antoniorusso2 <russoantonio303@outlook.it>
2025-04-21 11:21:13 +0200

    add colors to categories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,13 +5,14 @@ namespace Database\Seeders;
 use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
         $categories = [
             'Appetizers',
@@ -24,6 +25,7 @@ class CategorySeeder extends Seeder
             $newCategory = new Category();
 
             $newCategory->name = $category;
+            $newCategory->color = $faker->hexColor();
             $newCategory->save();
         }
     }
