09207ceeee2875b67d86c64756f5199dfd377189
antoniorusso2 <russoantonio303@outlook.it>
2025-05-03 15:59:22 +0200

    add api filter by name and category

--- app/Http/Controllers/Api/DishController.php
+++ app/Http/Controllers/Api/DishController.php
@@ -3,26 +3,53 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Dish;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class DishController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request): JsonResponse
     {
         //query build con parametri di ricerca
         //valore iniziale
-        $query = Dish::query()->with(['category', 'ingredients']);
+        $query = Dish::query();
 
-        if ($request->has('name')) {
+        if ($request->filled('name')) {
             // where name like %$request->name%
+            $name = trim($request->name);
+
             $query->where('name', 'like', '%' . $request->name . '%');
+        }
+
+        if ($request->filled('category') && $request->category != '') {
+
+            // dati parametro query separati da virgola
+            $formattedCategories = str_replace('-', ' ', $request->category);
+            $requestedCategories = explode(',', $formattedCategories);
+
+            // dd($requestedCategories);
+
+            // ottengo id delle categorie richieste
+            foreach ($requestedCategories as $category) {
+                //ritorna null in caso il campo categoria sia presente ma vuoto causando un errore nel momento in cui viene eseguito first() che in questo caso ritorna null e quindi si tenta di accedere a proprietà non esistenti
+                // $categoryIds[] = Category::where('name', $category)->first()->id;
+
+                $categoryIds = Category::whereIn('name', $requestedCategories)->pluck('id')->toArray();
+            }
+
+            // dd($categoryIds);
+
+            // concatenazione query
+            $query->whereIn('category_id', $categoryIds);
         }
 
         // paginazione 
         //limite elementi per pagina (default 10)
-        $limit = $request->has('limit') ? $request->limit : 10;
-        $dishes = $query->paginate($limit);
+        // $limit = $request->filled('limit') ? $request->limit : 10;
+        $limit = $request->input('limit', 10);
+        $dishes = $query->with(['category', 'ingredients'])->paginate($limit);
 
         return response()->json([
             'success' => true,
@@ -30,7 +57,7 @@ class DishController extends Controller
         ]);
     }
 
-    public function show(Dish $dish)
+    public function show(Dish $dish): JsonResponse
     {
         // aggiunta ingredienti
         $dish->load(['category', 'ingredients']);
