4cef73ac94de43b0c6f721ec136c8392cb9e71cf
antoniorusso2 <russoantonio303@outlook.it>
2025-05-10 20:40:00 +0200

    add trait to categories and ingredients

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasModelRoutes;
 
 class Category extends Model
 {
+    use HasModelRoutes;
+
     protected $hidden = ['created_at', 'updated_at'];
 
     public function dishes()
--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\HasModelRoutes;
 
 class Ingredient extends Model
 {
+    use HasModelRoutes;
+
     protected $hidden = ['pivot', 'quantity']; //nasconde la tabella pivot per la response api
 
     public function dishes()
